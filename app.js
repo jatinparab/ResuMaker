d7a98863a862baf7b23223bf0c0f146cc0b46d53
Jatin Parab <sulphurgfx@gmail.com>
2019-01-07 00:00:11 +0530

    Add Boilerplate Code

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyparser = require("body-parser");
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+
+const route = require('./routes/route');
+
+app.use('/api',route);
+app.use(cors());
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname,'public')));
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("Test");
+})
+
+app.listen(port, ()=>{
+    console.log("Listening on port: "+port);
+});
---
+++ routes/route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/data',(req,res,next)=>{
+    res.send('Retrieving data');
+});
+
+router.post('/add',(req,res,next)=>{
+    res.send('Adding data');
+});
+
+router.delete('/delete/:id',(req,res,next)=>{
+    res.send('Deleting data');
+});
+module.exports = router;
